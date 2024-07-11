e3303894a5caccb429285d63b6b3e9c94f9c4ad5
Tuhin <dastuhin.rnj@gmail.com>
2024-07-12 01:07:30 +0530

    Home page intial progress

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,12 @@ const dbConfig = {
 };*/
 
 // Serve static files (HTML, CSS, JS, images) from the frontend directory
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend', 'public', 'styles')));
+
+//Explicitly serving the stylesheet
+app.get('/style', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'public', 'styles', 'tailwind.css'));
+});
 
 // Handle clean URLs by serving specific HTML files
 app.get('/', (req, res) => {
